9ceaa5d08f5647c9d1c0b397166a14fc314c4024
Prabhukiran161 <prabhukiran161@gmail.com>
2025-02-07 20:31:48 +0530

    Updated the CLI Command

--- taskManager.js
+++ taskManager.js
@@ -1,22 +1,24 @@
+#!/usr/bin/env node
+
 import * as tasks from "./tasks.js";
 
-const command = process.argv[2];  // Command input
+const command = process.argv[2]; // Command input
 const argument1 = process.argv[3]; // First argument (e.g., task description or ID)
 const argument2 = process.argv[4]; // Second argument (status)
 
 switch (command) {
-    case "add":
-        tasks.addTask(argument1);
-        break;
-    case "list":
-        tasks.listTasks();
-        break;
-    case "update":
-        tasks.updateTask(Number(argument1), argument2);
-        break;
-    case "delete":
-        tasks.deleteTask(Number(argument1));
-        break;
-    default:
-        console.log("Unknown command. Use add, list, update, or delete.");
+  case "add":
+    tasks.addTask(argument1);
+    break;
+  case "list":
+    tasks.listTasks();
+    break;
+  case "update":
+    tasks.updateTask(Number(argument1), argument2);
+    break;
+  case "delete":
+    tasks.deleteTask(Number(argument1));
+    break;
+  default:
+    console.log("Unknown command. Use add, list, update, or delete.");
 }
